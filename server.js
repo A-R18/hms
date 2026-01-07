0cd02ee900c860896e30dc98e3de044512ea4914
pcUSER <gamchanger18@gmail.com>
2026-01-07 13:29:25 +0500

    added cors precaution for smooth running of things.

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
 const userRoutes = require("./app/routes/user.routes.js");
+const cors = require("cors");
 require("dotenv").config();
 app.use(express.json());
+app.use(cors())
+// app.use(cors({ origin: 'http://localhost:5173' })); // Vite default port is 5173
 // app.use(express.urlencoded({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use("/hospital/users", userRoutes);
