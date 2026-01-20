50cb4b46f709fd7a698bc68068beb4110167b5c2
pcUSER <gamchanger18@gmail.com>
2026-01-20 09:43:04 +0500

    accidental bug fixed (in login)

--- app/models/login.model.js
+++ app/models/login.model.js
@@ -3,7 +3,7 @@ const knex = require("knex")(require("../config/dbMod.js"));
 const authenticateUser = async (userEmail) => {
   const result = await knex("users")
     .where({ user_email: userEmail })
-    .select("users.id", "users.role_id", "users.email", "users.password")
+      .select("users.id", "users.role_ID", "users.user_email", "users.user_password")
     .first();
 
   return result;
