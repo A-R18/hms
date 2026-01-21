0cd21e8229956047b3521e24277d19769b370101
pcUSER <gamchanger18@gmail.com>
2026-01-21 09:16:15 +0500

    added doctor essentials in at login

--- app/controllers/login.controller.js
+++ app/controllers/login.controller.js
@@ -1,17 +1,20 @@
-const { authenticateUser, fetchUserRole, fetchUserPermissions } = require("../models/login.model.js");
+const { authenticateUser, fetchUserRole, fetchUserPermissions, FetchDocEssentials } = require("../models/login.model.js");
 const { generateToken } = require("../controllers/tokenGenerator.js");
 const bcrypt = require("bcrypt");
 const logUserIn = async (req, res) => {
   const mail = req.body.email;
   const pass = req.body.password;
   const authResponse = await authenticateUser(mail);
-
+  let doctorParticulars;
   // console.log("authenticated data is: ",authData);
   if (!authResponse) {
     return res.status(404).json({ message: "User doesn't exist!" });
   } else {
     const userRole = await fetchUserRole(authResponse.role_ID);
-    
+    if (userRole.role === "doctor") {
+       doctorParticulars = await FetchDocEssentials(authResponse.id);
+      //  console.log(doctorParticulars);
+    }
     const permissions = await fetchUserPermissions(authResponse.role_ID);
     const formattedPermz = [];
     permissions.map((p) => {
@@ -19,6 +22,7 @@ const logUserIn = async (req, res) => {
     });
     const authData = {
       id: authResponse.id,
+      name: authResponse.user_name,
       role_id: authResponse.role_ID,
       role: userRole.role,
       email: authResponse.user_email,
@@ -29,10 +33,17 @@ const logUserIn = async (req, res) => {
       return res.status(401).json({ message: "Invalid credentials" });
     } else {
       const token = generateToken(authData);
-      return res.status(202).json({
-        message: "Logged in successfully!",
-        ...authData, token: token, permissions: formattedPermz
-      });
+      if (userRole.role === "doctor") {
+        return res.status(202).json({
+          message: "Logged in successfully!",
+          ...authData, ...doctorParticulars, token: token, permissions: formattedPermz
+        });
+      } else {
+        return res.status(202).json({
+          message: "Logged in successfully!",
+          ...authData, token: token, permissions: formattedPermz
+        });
+      }
     }
   }
 };
--- app/models/login.model.js
+++ app/models/login.model.js
@@ -3,21 +3,27 @@ const knex = require("knex")(require("../config/dbMod.js"));
 const authenticateUser = async (userEmail) => {
   const result = await knex("users")
     .where({ user_email: userEmail })
-      .select("users.id", "users.role_ID", "users.user_email", "users.user_password")
+    .select("users.id", "users.user_name", "users.role_ID", "users.user_email", "users.user_password")
     .first();
 
   return result;
 };
 
-const fetchUserRole = (userRoleID)=>{
-return knex("roles").select("roles.role").where({id:userRoleID}).first();
+const fetchUserRole = (userRoleID) => {
+  return knex("roles").select("roles.role").where({ id: userRoleID }).first();
 }
 
 const fetchUserPermissions = (userRoleID) => {
-    return knex("permissions").where({ role_ID: userRoleID })
+  return knex("permissions").where({ role_ID: userRoleID })
     .join("modules", "permissions.module_id", "modules.id")
     .join("privileges", "permissions.privilege_id", "privileges.id")
     .select("modules.module", "privileges.privilege");
 }
 
-module.exports = { authenticateUser, fetchUserRole, fetchUserPermissions };
+const FetchDocEssentials = (docUID) => {
+  return knex("doctors").join("doctor_specialities", "doctors.spec_ID", "doctor_specialities.id").select("doctor_specialities.speciality").first();
+}
+
+
+
+module.exports = { authenticateUser, fetchUserRole, fetchUserPermissions, FetchDocEssentials };
